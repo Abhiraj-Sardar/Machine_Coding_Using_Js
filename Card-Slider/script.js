4c712f55578180e08c4b97965ac18f1b15c8f759
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2025-03-11 00:50:38 +0530

    updated Card-Slider

---
+++ Card-Slider/script.js
@@ -0,0 +1,5 @@
+var left = document.querySelector(".left");
+var right = document.querySelector(".right");
+var cardContainer = document.querySelector(".card");
+// cardContainer.style.transform="translateX()";
+cardContainer.style.backgroundColor="black";

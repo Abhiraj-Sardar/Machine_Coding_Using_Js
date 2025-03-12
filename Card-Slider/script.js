dff9f85d74d18d9a5a316cc39f0ea73e05a1248d
Abhiraj Sardar <abhirajsardar2003@gmail.com>
2025-03-13 00:37:25 +0530

    updated js file

--- Card-Slider/script.js
+++ Card-Slider/script.js
@@ -1,5 +1,28 @@
 var left = document.querySelector(".left");
 var right = document.querySelector(".right");
-var cardContainer = document.querySelector(".card");
-// cardContainer.style.transform="translateX()";
-cardContainer.style.backgroundColor="black";
+var subContainer = document.querySelector(".sub-container");
+var val=0;
+
+left.addEventListener("click",()=>{
+    let limit=(-1)*(4*1400); //limiting the slide transition
+    if(val>limit)
+    {
+        subContainer.style.transform=`translateX(-${val}px)`;
+        val+=1400;
+    }
+    else{
+        val=0; //go to reverse
+    }
+})
+
+right.addEventListener("click",()=>{
+    // let limit=(-1)*(3*1400); //limiting the slide transition
+    // if(val>=limit)
+    // {
+        subContainer.style.transform=`translateX(${val}px)`;
+        val+=1400;
+    // }
+    // else{
+    //     val=0; //go to reverse
+    // }
+})
